56cc7422506864659c6ede525832bc94f1597e20
Ezher Memeti <ezher.memeti@gmail.com>
2024-05-20 19:05:16 +0300

    2-opt daha hızlı çalışması için optimize edildi

--- Main.java
+++ Main.java
@@ -1,11 +1,12 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class Main {
 
+    private static final int MAX_ITERATIONS_WITHOUT_IMPROVEMENT = 1000;
+    private static final int MAX_ITERATIONS = 10000;
+    private static final double INITIAL_TEMPERATURE = 10000;
+    private static final double COOLING_RATE = 0.995;
     public static void main(String[] args) {
         //READ THE INPUT FILE
         String inputFile = "input1.txt";
@@ -43,31 +44,25 @@ public class Main {
         //:::::TEST:::::
         System.out.println("--------------TEST-------------");
         System.out.println("tour1: - BEFORE 2-OPT");
-        for (City city : tour1) {
-            System.out.print(" , "+(city.getId()));
-        }
-        System.out.println();
-        System.out.println("tour2:");
-        for (City city : tour2) {
-            System.out.print(" , "+(city.getId()));
-        }
         System.out.println();
         int totalDistance = calculateTourDistance(tour1) + calculateTourDistance(tour2);
         System.out.println("total distance: "+totalDistance);
 
+
+        tour1=simulatedAnnealing(tour1);
+        tour2=simulatedAnnealing(tour2);
+
         // Apply 2-opt to improve the tours
+        long l = System.currentTimeMillis();
         tour1 = twoOpt(tour1);
         tour2 = twoOpt(tour2);
+        long e =System.currentTimeMillis();
+
+        System.out.println("Execution time:"+(e-l/1000000));
+
         //TEST
         System.out.println("\n\ntour1: - AFTER 2-Opt Algorithm");
-        for (City city : tour1) {
-            System.out.print(" , "+(city.getId()));
-        }
-        System.out.println();
-        System.out.println("tour2:");
-        for (City city : tour2) {
-            System.out.print(" , "+(city.getId()));
-        }
+
         System.out.println();
         totalDistance = calculateTourDistance(tour1) + calculateTourDistance(tour2);
         System.out.println("total distance: "+totalDistance);
@@ -89,8 +84,8 @@ public class Main {
         ArrayList<City> tour2 = new ArrayList<>();
         // Choose a random city to start
         Random random = new Random();
-        City startCityTour1 = cities.get(random.nextInt(cities.size()));
-        City startCityTour2 = cities.get(random.nextInt(cities.size()));
+        City startCityTour1 = cities.get(0);
+        City startCityTour2 = cities.get(cities.size()-1);
         tour1.add(startCityTour1);
         tour2.add(startCityTour2);// add the start city to the arraylist
 
@@ -142,43 +137,112 @@ public class Main {
         return new ArraylistHolder(tour1, tour2);
     }
 
-
+//
     // 2-OPT ALGORITHM
-    public static List<City> twoOpt(List<City> tour) {
-        int n = tour.size();
-        boolean improvement = true;
-        int bestDistance = calculateTourDistance(tour);
+public static List<City> twoOpt(List<City> tour) {
+    int n = tour.size();
+    boolean improvement = true;
+    int bestDistance = calculateTourDistance(tour);
+    int iterationsWithoutImprovement = 0;
 
-        while (improvement) {
-            improvement = false;
-            for (int i = 1; i < n - 1; i++) {
-                for (int j = i + 1; j < n; j++) {
-                    List<City> newTour = new ArrayList<>(tour);
-                    Collections.reverse(newTour.subList(i, j));
-                    int newDistance = calculateTourDistance(newTour);
-                    if (newDistance < bestDistance) {
-                        tour = newTour;
-                        bestDistance = newDistance;
-                        improvement = true;
-                        break;
-                    }
-                }
-                if (improvement) {
-                    break;
+    while (improvement && iterationsWithoutImprovement < MAX_ITERATIONS_WITHOUT_IMPROVEMENT) {
+        improvement = false;
+        for (int i = 1; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int newDistance = calculateNewTourDistanceAfter2OptMove(tour, i, j, bestDistance);
+                if (newDistance < bestDistance) {
+                    perform2OptMove(tour, i, j);
+                    bestDistance = newDistance;
+                    improvement = true;
+                    iterationsWithoutImprovement = 0; // Reset counter
+                    break; // Exit inner loop
                 }
             }
+            if (improvement) {
+                break; // Exit outer loop
+            }
         }
-        return tour;
+        iterationsWithoutImprovement++;
+    }
+    return tour;
+}
+
+    public static int calculateNewTourDistanceAfter2OptMove(List<City> tour, int i, int j, int currentDistance) {
+        int n = tour.size();
+
+        // Get the cities involved in the 2-opt move
+        City city1 = tour.get((i - 1 + n) % n); // Previous city of i, considering circular tour
+        City city2 = tour.get(i);
+        City city3 = tour.get(j);
+        City city4 = tour.get((j + 1) % n); // Next city of j, considering circular tour
+
+        // Calculate the distances of edges before and after the 2-opt move
+        int oldDistance1 = calculateDistance(city1, city2);
+        int oldDistance2 = calculateDistance(city3, city4);
+        int newDistance1 = calculateDistance(city1, city3);
+        int newDistance2 = calculateDistance(city2, city4);
+
+        // Calculate the change in tour distance
+        int distanceChange = (newDistance1 + newDistance2) - (oldDistance1 + oldDistance2);
+
+        // Calculate the new tour distance
+        return currentDistance + distanceChange;
+    }
+    public static List<City> simulatedAnnealing(List<City> tour) {
+        double temperature = INITIAL_TEMPERATURE;
+        List<City> currentTour = new ArrayList<>(tour);
+        List<City> bestTour = new ArrayList<>(tour);
+        int bestDistance = calculateTourDistance(tour);
+
+        while (temperature > 1) {
+            List<City> newTour = new ArrayList<>(currentTour);
+            // Apply a random 2-opt move
+            int i = 1 + (int) (Math.random() * (newTour.size() - 2));
+            int j = i + 1 + (int) (Math.random() * (newTour.size() - i - 1));
+            perform2OptMove(newTour, i, j);
+
+            int currentDistance = calculateTourDistance(currentTour);
+            int newDistance = calculateTourDistance(newTour);
+
+            // Accept new tour if it's better or with a probability depending on the temperature
+            if (acceptanceProbability(currentDistance, newDistance, temperature) > Math.random()) {
+                currentTour = newTour;
+                if (newDistance < bestDistance) {
+                    bestTour = newTour;
+                    bestDistance = newDistance;
+                }
+            }
+
+            // Cool down
+            temperature *= COOLING_RATE;
+        }
+
+        return bestTour;
     }
 
+    // Acceptance probability function for simulated annealing
+    public static double acceptanceProbability(int currentDistance, int newDistance, double temperature) {
+        if (newDistance < currentDistance) {
+            return 1.0;
+        }
+        return Math.exp((currentDistance - newDistance) / temperature);
+    }
 
-    // FUNCTION TO CALCULATE THE TOTAL DISTANCE OF THE TOUR
+    // Function to perform a 2-opt move on the tour
+    public static void perform2OptMove(List<City> tour, int i, int j) {
+        while (i < j) {
+            Collections.swap(tour, i, j);
+            i++;
+            j--;
+        }
+    }
+
     public static int calculateTourDistance(List<City> tour) {
         int totalDistance = 0;
         for (int i = 0; i < tour.size() - 1; i++) {
             totalDistance += calculateDistance(tour.get(i), tour.get(i + 1));
         }
-        totalDistance+=calculateDistance(tour.get(0),tour.get(tour.size()-1));
+        totalDistance += calculateDistance(tour.get(0), tour.get(tour.size() - 1));
         return totalDistance;
     }
 
@@ -213,4 +277,5 @@ public class Main {
             e.printStackTrace();
         }
     }
+
 }
